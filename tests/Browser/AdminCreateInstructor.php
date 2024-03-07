c23c13ed3c67a8ffd74acaca1191c289fc97e48a
Constantin2105 <94076778+Constantin2105@users.noreply.github.com>
2024-03-07 22:00:43 +0100

    add test for admin create instructor

---
+++ tests/Browser/AdminCreateInstructor.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Browser;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class AdminCreateInstructor extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/login');
+            $browser->type('email', 'admin@ulearn.com')
+                    ->type('password', 'secret')
+                    ->click('button[type="submit"]');
+
+            $browser->visit('/admin/user-form')
+                    ->type('first_name', 'John')
+                    ->type('last_name', 'Doe')
+                    ->type('email', 'johndoe@example.com')
+                    ->check('roles[]', 'instructor')
+                    ->click('#inputBasicActive')
+                    ->type('password', 'secret')
+                    ->click('button[type="submit"]');
+
+            $browser->visit('/admin/users')
+                    ->assertSee('John Doe');
+            
+        });
+    }
+}
